63593017c6fb4980d1425de5ceed6ca37995f7f3
Webert Dias <wdwebert@gmail.com>
2023-03-22 08:33:14 -0300

    Relógio pronto

--- relogio.js
+++ relogio.js
@@ -1,16 +1,21 @@
-const horas = document.getElementById("#horas");
-const minutos = document.getElementById("#minutos");
-const segundos= document.getElementById("segundos");
+const horas = document.querySelector("#horas");
+const minutos = document.querySelector("#minutos");
+const segundos = document.querySelector("#segundos");
 
+setInterval(relogio, 1000);
 
-function relogio(){
-     let data = new Date();
-     let hora = data.getHours;
-     let minuto = data.getMinutes;
-     let segundo = data.getSeconds;
+function relogio() {
 
-    horas.innerHTML = hora;
-    minutos.innerHTML = minuto;
-    segundos.innerHTML = segundo;
+    let data = new Date();
+    let hora = data.getHours();
+    let minuto = data.getMinutes();
+    let segundo = data.getSeconds();
 
+    horas.innerHTML = formato(hora);
+    minutos.innerHTML = formato(minuto);
+    segundos.innerHTML = formato(segundo);
+}
+
+function formato(horario) {
+    return horario < 10 ? "0" + horario : horario;
 }
